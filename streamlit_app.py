0e03783a57c1526ea9c6c7f0fcad441b5e26bda3
CarlosAlbertoLinares <51976293+CarlosAlbertoLinares@users.noreply.github.com>
2023-07-21 09:29:11 -0400

    Update streamlit_app.py
    eliminated the raw json line related to a streamlit.text

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,8 +20,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #display the table on the page
 streamlit.dataframe(fruits_to_show)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")  ##to make contact with the data over html
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")  ##to make contact with the data over html
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
