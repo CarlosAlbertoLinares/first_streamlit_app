a8df218847524a457eec635ccce8f1b3c8c14e93
CarlosAlbertoLinares <51976293+CarlosAlbertoLinares@users.noreply.github.com>
2023-07-21 09:01:29 -0400

    Update streamlit_app.py
    changed the app header to Fruityvice Fruit Advice! and added .json() method to the last streamlit.text

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('🍌🥭 Fruityvice Fruit Advice! 🥝🍇')
 
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
@@ -21,7 +21,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")  ##to make contact with the data over html
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 
 
 
