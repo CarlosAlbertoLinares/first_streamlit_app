179c905d5250bd01a00318f4e69d1243dcc69824
CarlosAlbertoLinares <51976293+CarlosAlbertoLinares@users.noreply.github.com>
2023-07-25 13:50:01 -0400

    Update streamlit_app.py
    corrected a type in the word streamlit line 38

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ my_cur.execute("SELECT * FROM fruit_load_list")
 my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
-add_my_fruit = stramlit.text_input('What fruit would you like to add ?','None')
+add_my_fruit = streamlit.text_input('What fruit would you like to add ?','None')
 streamlit.write('Thanks for adding',add_my_fruit)
 
 
