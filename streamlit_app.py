20fe6bbc8dcedec713ad194c794cd2dc320424cf
CarlosAlbertoLinares <51976293+CarlosAlbertoLinares@users.noreply.github.com>
2023-07-27 14:10:08 -0400

    Update streamlit_app.py
    added a functions and eliminated the try catch block

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,15 +28,20 @@ streamlit.dataframe(fruits_to_show)
 
 #new section to display fruityviceapi response
 streamlit.header('🍌🥭 Fruityvice Fruit Advice! 🥝🍇')
+
+def get_fruityvice_data(this_fruit_choice):
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)  ##to make contact with the data over html
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        return fruityvice_normalized
 try:
     fruit_choice = streamlit.text_input('What fruit would you like information about ?', 'Kiwi')
     if not fruit_choice:
       streamlit.error("Please select a fruit to get information.")
     else:
+        back_from_function = get_fruityvice_data(fruit_choice)
+        streamlit.dataframe(back_from_function)
 #streamlit.write('The user entered', fruit_choice)
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)  ##to make contact with the data over html
-        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-        streamlit.dataframe(fruityvice_normalized)
+
 except URLError as e:
     streamlit.error()
 
