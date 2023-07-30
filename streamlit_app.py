1b7aaf484f5b56110d03d78bc8b720ce27804933
CarlosAlbertoLinares <51976293+CarlosAlbertoLinares@users.noreply.github.com>
2023-07-30 13:05:43 -0400

    Update streamlit_app.py
    added functions to execute the query from snowflake and another to show the data in the streamlit application.

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,19 +46,34 @@ except URLError as e:
     streamlit.error()
 
 
-streamlit.stop()
-
-
+streamlit.header("The fruit load list contains:")
+#snowflake related functions
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+def get_fruit_load_list():
+        with my_cnx.cursor() as my_cur:
+                my_cur.execute("SELECT * FROM fruit_load_list")
+                return my_cur.fetchall()
+if stramlit.button('Get Fruit Load List'):
+        my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+        my_data_rows = get_fruit_load_list()
+        streamlit.dataframe(my_data_rows)
+                
+
+
+"""
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM fruit_load_list")
 my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
+
 streamlit.dataframe(my_data_row)
+"""
+"""
 add_my_fruit = streamlit.text_input('What fruit would you like to add ?','None')
 streamlit.write('Thanks for adding',add_my_fruit)
 
 my_cur.execute("INSERT INTO FRUIT_LOAD_LIST values ('from streamlit')")
+"""
 
 
 
